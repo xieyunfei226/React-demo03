d46519efef501156ea77467ee6c0974023c3a56e
xieyunfei226 <1625453189@qq.com>
2020-10-23 15:09:16 +0800

    demo03

---
+++ precache-manifest.392c2a703daba5658e08418f6b96f59c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a7dabcb40e4639589332c4ea352298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d3cc1b9d77bc37fc97",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b15cc57f30cd2ea815cb",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "76d3cc1b9d77bc37fc97",
+    "url": "/static/js/2.1607856b.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.1607856b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15cc57f30cd2ea815cb",
+    "url": "/static/js/main.71f34751.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
